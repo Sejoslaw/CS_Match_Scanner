f57258316de0fdfcc1404400f326faef19db2c9e
sejoslaw <k.dobrzynski94@gmail.com>
2024-01-03 08:43:12 +0100

    !cleaning;

--- Scanner.cs
+++ Scanner.cs
@@ -1,12 +1,12 @@
 namespace CsMatchScanner;
 
-using CsMatchScanner.Contexts;
+using Contexts;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 public class Scanner
 {
-    public ScannerContext Context { get; } = new ScannerContext();
+    public ScannerContext Context { get; } = new();
     public IWebDriver Driver { get; }
 
     public Scanner()
